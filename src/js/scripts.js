cd0dc10ed367679a7d2e6cc851e9cce337ae2329
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2019-12-11 23:01:30 +0200

    Remove unnecessary files

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,5 @@
 import MobileMenu from './modules/MobileMenu';
-// import Spoiler from './modules/Spoiler';
 import Map from './modules/Map';
 
 const menu = new MobileMenu();
-// const spoiler = new Spoiler();
 const map = new Map();

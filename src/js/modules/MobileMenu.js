63ba140815572e4855ca1ed125dec8d37e56983a
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2019-12-31 17:35:53 +0200

    Add main-nav behavior

--- src/js/modules/MobileMenu.js
+++ src/js/modules/MobileMenu.js
@@ -1,13 +1,10 @@
 class MobileMenu {
   constructor() {
-    this.headerTop = document.querySelector('.header__top');
     this.menuButton = document.querySelector('.menu-button');
     this.mainNav = document.querySelector('.main-nav');
 
-    if (this.headerTop) {
-      this.resetDOM();
-      this.addEvents();
-    }
+    this.resetDOM();
+    this.addEvents();
   };
 
   addEvents() {
@@ -17,13 +14,11 @@ class MobileMenu {
   };
 
   resetDOM() {
-    this.headerTop.classList.remove('header__top--green');
     this.mainNav.classList.add('main-nav--hidden');
     this.menuButton.classList.remove('menu-button--close');
   };
 
   switchMenu() {
-    this.headerTop.classList.toggle('header__top--green');
     this.mainNav.classList.toggle('main-nav--hidden');
     this.menuButton.classList.toggle('menu-button--close');
   };
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,4 +2,4 @@ import MobileMenu from './modules/MobileMenu';
 import Map from './modules/Map';
 
 const menu = new MobileMenu();
-const map = new Map();
+// const map = new Map();
